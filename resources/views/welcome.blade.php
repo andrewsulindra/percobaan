173aaeeb6fe3c580e7bdce2a1042a48f98560584
andrewsulindra <andrewsulindra@atlassian.com>
2020-05-20 18:34:28 +0700

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravelsss
+                    Laravelsss test commit
                 </div>
 
                 <div class="links">
